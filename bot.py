1f2090b7e2b9ef486c310e49e086444780f8bebf
Christian <cabrokwa11@gmail.com>
2021-11-15 22:26:18 +0000

    Bare functionality working

--- bot.py
+++ bot.py
@@ -1,10 +1,37 @@
-import googletrans
-#print(googletrans.LANGUAGES)
-
+import googletrans, telegram.ext
 from googletrans import Translator
 
 translator = Translator()
 
-print(translator.translate("Hola, mi nombre es Christian"))
+with open("token.txt", "r") as token:
+	TOKEN = token.read()
 
-print(translator.detect("Hola, mi nombre es Christian"))
+def translating(text):
+	trans_text = translator.translate(text)
+	return trans_text
+
+def start(update, context):
+	update.message.reply_text("Hello, welcome to Daquiver's Language Translator ")
+#print(googletrans.LANGUAGES)
+
+def handle_message(update, context):
+	update.message.reply_text(f"You typed {update.message.text} and the transaltion is {translating(update.message.text)} ")
+
+
+updater = telegram.ext.Updater(TOKEN, use_context=True)
+disp = updater.dispatcher
+
+disp.add_handler(telegram.ext.CommandHandler("start", start))
+disp.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, handle_message))
+
+
+updater.start_polling()
+updater.idle()
+
+
+
+
+# With googletrans the methods are
+# translate
+# detect
+# LANGUAGES
