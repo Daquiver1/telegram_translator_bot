4fc4e36b307fa25c098e4973e1a278630411e5cb
Christian <cabrokwa11@gmail.com>
2021-11-16 18:37:45 +0000

    Cleaned output

--- bot.py
+++ bot.py
@@ -1,5 +1,3 @@
-# TODO: Output the languages in a newline format
-# TODO: Clean how translations are represented.
 # TODO: Let the user specify the transaltion language.
 # TODO: Let the user speicfy the source language.
 # TODO: Auto detect the source language.
@@ -18,8 +16,24 @@ for i in googletrans.LANGUAGES.values():
 	languages.append(i)
 
 def translating(text):
-	trans_text = translator.translate(text)
-	return trans_text
+	return translator.translate(text).text
+
+def src_lang(text):
+	short_lang =translator.translate(text).src
+	for i,v in googletrans.LANGUAGES.items():
+		if i == short_lang:
+			lang = v
+
+	return lang
+
+
+def dest_lang(text):
+	short_lang1= translator.translate(text).dest
+	for i,v in googletrans.LANGUAGES.items():
+		if i == short_lang1:
+			lang = v
+
+	return lang
 
 def show_langs():
 	for i in range(len(languages)):
@@ -27,7 +41,6 @@ def show_langs():
 
 def start(update, context):
 	update.message.reply_text("Hello!, welcome to Daquiver's Language Translator ")
-#print(googletrans.LANGUAGES)
 
 def langs(update, context):
 	update.message.reply_text(f"The languages we support are ")
@@ -35,7 +48,12 @@ def langs(update, context):
 		update.message.reply_text(languages[i])
 
 def handle_message(update, context):
-	update.message.reply_text(f"You typed {update.message.text} and the transaltion is {translating(update.message.text)} ")
+	update.message.reply_text(f"""
+	You typed: {update.message.text}
+	Source Language: {src_lang(update.message.text)}
+	Dest Language: {dest_lang(update.message.text)} 
+	Transaltion: {translating(update.message.text)}
+	""")
 
 
 updater = telegram.ext.Updater(TOKEN, use_context=True)
